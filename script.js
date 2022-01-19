cea51ea0ba55d2bbe1caa52f2b95f3cfb55d6537
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-18 19:29:45 -0600

    started to add a question template

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+let questions = [
+    {
+        question: "What house is the best?",
+        answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
+        correctAnswer: "Gryffindor",
+    }
+]
