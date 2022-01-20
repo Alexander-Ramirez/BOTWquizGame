2577ec44bbf7c5223cf8b34150fec930a1d7f771
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-20 15:49:10 -0600

    finished the questions

--- script.js
+++ script.js
@@ -6,5 +6,25 @@ let questions = [
         question: "What house is the best?",
         answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
         correctAnswer: "Gryffindor",
-    }
+    },
+    {
+        question: "Which Quidditch team is the worst?",
+        answers: ["Montrose Magpies", "Wimbourne Wasps", "Chudley Cannons", "Appleby Arrows"],
+        correctAnswer: "Chudley Cannons",
+    },
+    {
+        question: "What is Gilderoy Lockhart's favorite color?",
+        answers: ["Gold", "Royal Blue", "Purple", "Lilac"],
+        correctAnswer: "Lilac",
+    },
+    {
+        question: "What house is Harry Potter in?",
+        answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
+        correctAnswer: "Gryffindor",
+    },
+    {
+        question: "What house is the nicest?",
+        answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
+        correctAnswer: "Hufflepuff",
+    },
 ]
