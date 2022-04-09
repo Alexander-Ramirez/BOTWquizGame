fc51104961677cdc12f8c107c79653c4375fa81a
Alex.Ramirez <aleramirez201@gmail.com>
2022-04-09 16:33:57 -0500

    changed the catagory and changed/added more questions

--- script.js
+++ script.js
@@ -4,29 +4,54 @@ let timeEl = document.querySelector("#timer");
 
 let questions = [
     {
-        question: "What house is the best?",
-        answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
-        correctAnswer: "Gryffindor",
+        question: "How many hearts do you need to get the master sword?",
+        answers: ["12", "13", "14", "11"],
+        correctAnswer: "13",
     },
     {
-        question: "Which Quidditch team is the worst?",
-        answers: ["Montrose Magpies", "Wimbourne Wasps", "Chudley Cannons", "Appleby Arrows"],
-        correctAnswer: "Chudley Cannons",
+        question: "What is the max amount of stamina rings that you can have?",
+        answers: ["3", "2.5", "2", "4"],
+        correctAnswer: "3",
     },
     {
-        question: "What is Gilderoy Lockhart's favorite color?",
-        answers: ["Gold", "Royal Blue", "Purple", "Lilac"],
-        correctAnswer: "Lilac",
+        question: "Is there a time limit to beat the game?",
+        answers: ["yes", "no"],
+        correctAnswer: "no",
     },
     {
-        question: "What house is Harry Potter in?",
-        answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
-        correctAnswer: "Gryffindor",
+        question: "What is the kings name?",
+        answers: ["King Daltus", "King Harkinian.", "King Daphnes", "King Rhoam"],
+        correctAnswer: "King Rhoam",
     },
     {
-        question: "What house is the nicest?",
-        answers: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
-        correctAnswer: "Hufflepuff",
+        question: "Can you get the master sword without meeting Impa?",
+        answers: ["yes", "no"],
+        correctAnswer: "yes",
+    },
+    {
+        question: "How many kokorok seeds are there?",
+        answers: ["1000", "900", "700", "800"],
+        correctAnswer: "900",
+    },
+    {
+        question: "How many shrines are there?",
+        answers: ["120", "100", "150", "80"],
+        correctAnswer: "120",
+    },
+    {
+        question: "How many great fairys are there?",
+        answers: ["3", "5", "4", "2"],
+        correctAnswer: "4",
+    },
+    {
+        question: "What is the base damage the master sword does?",
+        answers: ["60", "20", "30", "50"],
+        correctAnswer: "30",
+    },
+    {
+        question: "How much do diamonds sell for?",
+        answers: ["250", "1000", "750", "500"],
+        correctAnswer: "500",
     },
 ]
 
