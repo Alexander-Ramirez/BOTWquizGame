7644574f78d5c19c9f011b32f699708aee9af862
Alex.Ramirez <aleramirez201@gmail.com>
2022-04-15 18:45:24 -0500

    this loops checks too see if the user the question correct or not

--- script.js
+++ script.js
@@ -86,6 +86,19 @@ function showResults(){
     const answerContainers = quizContainer.querySelectorAll('.answers');
 
     let numCorrect = 0;
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        } else{
+            answerContainers[questionNumber].style.color = 'red';
+        };
+    });
 };
 
 // display quiz right away
