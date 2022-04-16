cec08069196afcc0b8eb73947356198389266d24
Alex.Ramirez <aleramirez201@gmail.com>
2022-04-15 20:25:40 -0500

    added showslide function to be able to switch what question the user is currently on

--- script.js
+++ script.js
@@ -48,6 +48,26 @@ function showResults(){
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`
 };
 
+function showSlide(n){
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+
+    if(currentSlide === 0){
+        previousButton.style.display = 'none';
+    } else{
+        previousButton.style.display = 'inline-block';
+    };
+
+    if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    } else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    };
+};
+
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
